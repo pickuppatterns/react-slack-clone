c31ffdd547b3cf999ecdafef509d834ed40aed92
pickuppatterns <pickup.patterns@gmail.com>
2020-03-13 13:55:15 -0700

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/storage';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBZpc-x2sqCTqqTBpFH_O0IcyGMHCt2Fco",
+    authDomain: "a-react-clone-slack.firebaseapp.com",
+    databaseURL: "https://a-react-clone-slack.firebaseio.com",
+    projectId: "a-react-clone-slack",
+    storageBucket: "a-react-clone-slack.appspot.com",
+    messagingSenderId: "529959966918",
+    appId: "1:529959966918:web:364f7548c5726ba5bedfc7",
+    measurementId: "G-GN5MNTJCTB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  export default firebase;
